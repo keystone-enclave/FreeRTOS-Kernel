af36645de1d93dae66ae54a5949d88e3f545a3b0
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-11-19 00:55:53 +0000

    Added back other tasks

--- main/src/main.c
+++ main/src/main.c
@@ -19,41 +19,41 @@ extern void freertos_risc_v_trap_handler(void);
 
 uint8_t ucHeap[ configTOTAL_HEAP_SIZE ];
 
-// static void taskTestFn(void *pvParameters); 
-// static void taskTestFn2(void *pvParameters);
-// TaskHandle_t taskTest; 
-// TaskHandle_t taskTest2;
+static void taskTestFn(void *pvParameters); 
+static void taskTestFn2(void *pvParameters);
+TaskHandle_t taskTest; 
+TaskHandle_t taskTest2;
 TaskHandle_t taskCLI;
 
-// TaskHandle_t enclave1; 
-// TaskHandle_t enclave2; 
+TaskHandle_t enclave1; 
+TaskHandle_t enclave2; 
 
 
 int main( void )
 {
 
-   // extern uintptr_t _rtos_base; 
-   // extern uintptr_t _rtos_end; 
+   extern uintptr_t _rtos_base; 
+   extern uintptr_t _rtos_end; 
 
-   // extern char * _task_1_start; 
-   // extern char *_task_1_end; 
+   extern char * _task_1_start; 
+   extern char *_task_1_end; 
 
-   // extern char * _task_2_start; 
-   // extern char *_task_2_end; 
-
-
-   // size_t elf_size = (char *) &_task_1_end - (char *) &_task_1_start;
+   extern char * _task_2_start; 
+   extern char *_task_2_end; 
 
 
-   // printf("Free RTOS booted at 0x%p-0x%p!\n", &_rtos_base, &_rtos_end); 
-   // printf("Enclave 1 at 0x%p-0x%p!\n", &_task_1_start, &_task_1_end); 
-   // printf("Enclave 2 at 0x%p-0x%p!\n", &_task_2_start, &_task_2_end);
+   size_t elf_size = (char *) &_task_1_end - (char *) &_task_1_start;
 
-   // xTaskCreateEnclave((uintptr_t) &_task_1_start, elf_size, "fibonacci", 30, &enclave1); 
-   // xTaskCreateEnclave((uintptr_t) &_task_2_start, elf_size, "attest", 30, &enclave2); 
+
+   printf("Free RTOS booted at 0x%p-0x%p!\n", &_rtos_base, &_rtos_end); 
+   printf("Enclave 1 at 0x%p-0x%p!\n", &_task_1_start, &_task_1_end); 
+   printf("Enclave 2 at 0x%p-0x%p!\n", &_task_2_start, &_task_2_end);
+
+   xTaskCreateEnclave((uintptr_t) &_task_1_start, elf_size, "fibonacci", 30, &enclave1); 
+   xTaskCreateEnclave((uintptr_t) &_task_2_start, elf_size, "attest", 30, &enclave2); 
    
-   // xTaskCreate(taskTestFn, "taskTest1", configMINIMAL_STACK_SIZE, NULL, 25, &taskTest); 
-   // xTaskCreate(taskTestFn2, "taskTest2", configMINIMAL_STACK_SIZE, NULL, 21, &taskTest2);
+   xTaskCreate(taskTestFn, "taskTest1", configMINIMAL_STACK_SIZE, NULL, 25, &taskTest); 
+   xTaskCreate(taskTestFn2, "taskTest2", configMINIMAL_STACK_SIZE, NULL, 21, &taskTest2);
 
    xTaskCreate( vCommandConsoleTask, "CLI", configMINIMAL_STACK_SIZE, NULL, 1, &taskCLI );
 
@@ -70,17 +70,17 @@ int main( void )
 	return 1; 
 }
 
-// static void taskTestFn(void *pvParameters){
-//    printf("Untrusted Task 1 DONE\n"); 
-//    syscall_task_return();
-// }
+static void taskTestFn(void *pvParameters){
+   printf("Untrusted Task 1 DONE\n"); 
+   syscall_task_return();
+}
 
-// static void taskTestFn2(void *pvParameters){
-//    printf("Untrusted Task 2 before yield\n");
-//    syscall_task_yield(); 
-//    printf("Untrusted Task 2 DONE\n");
-//    syscall_task_return();
-// }
+static void taskTestFn2(void *pvParameters){
+   printf("Untrusted Task 2 before yield\n");
+   syscall_task_yield(); 
+   printf("Untrusted Task 2 DONE\n");
+   syscall_task_return();
+}
 
 /*-----------------------------------------------------------*/
 
