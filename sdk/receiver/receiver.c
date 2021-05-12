a683fb9b8aa560fdd23fbd0d6ddbc9983fc69125
Alex Thomas <alexthomas@berkeley.edu>
2021-05-12 21:05:56 +0000

    Add fix

--- sdk/receiver/receiver.c
+++ sdk/receiver/receiver.c
@@ -7,15 +7,13 @@
 #include "msg_test.h"
 
 char send_buf[DATA_SIZE] = "alex";
-char recv_buf[DATA_SIZE];
+char recv_buf[DATA_SIZE] = {0};
 
 void EAPP_ENTRY eapp_entry(int SENDER_TID){
 
-    // int x = 5; 
-    // int y = 3; 
-
-    sbi_recv(SENDER_TID, recv_buf, DATA_SIZE, YIELD);
-    // while(sbi_recv(SENDER_TID, recv_buf, DATA_SIZE, YIELD));
+    // sbi_recv(SENDER_TID, recv_buf, DATA_SIZE, YIELD);
+    while(sbi_recv(SENDER_TID, recv_buf, DATA_SIZE, YIELD));
     sbi_send(SENDER_TID, send_buf, DATA_SIZE, YIELD); 
+    syscall_task_yield();
     syscall_task_return();
 }
